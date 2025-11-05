7a4a82c98c61b1691cafa25be4d61a030df4a800
Ryan Guan <ryanwguan@gmail.com>
2025-11-04 19:42:10 -0800

    Added quotes database

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import { db } from "./firebaseConfig.js";
+import { doc, onSnapshot, getDoc } from "firebase/firestore";
 
+function readQuote(day) {
+    const quoteDocRef = doc(db, "quotes", day); // Get a reference to the document
+
+    onSnapshot(quoteDocRef, (docSnap) => { // Listen for real-time updates
+        if (docSnap.exists()) {
+            document.getElementById("quote-goes-here").innerHTML = docSnap.data().quote;
+        } else {
+            console.log("No such document!");
+        }
+    }, (error) => {
+        console.error("Error listening to document: ", error);
+    });
+}
 function sayHello() {
     
 }
+readQuote("tuesday");
 // document.addEventListener('DOMContentLoaded', sayHello);
