e7d73a2433c8b19a2f8cd0ebbc07d7c98623dedf
Ryan Guan <ryanwguan@gmail.com>
2025-11-12 11:31:32 -0800

    Edited Profile.html to fetch from database

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -4,7 +4,7 @@ class SiteFooter extends HTMLElement {
             <footer class="navbar justify-content-evenly fixed-bottom bg-body-tertiary">
                 <div class="container text-center">
                     <a class="nav-link" href="/main.html"><span class="material-icons" type="button" style="font-size: 48px;">home</span><br>Home</a>
-                    <a class="nav-link" href=""><span class="material-icons" type="button" style="font-size: 48px;">fitness_center</span><br>???</a>
+                    <a class="nav-link" href="/session.html"><span class="material-icons" type="button" style="font-size: 48px;">fitness_center</span><br>Session</a>
                     <a class="nav-link" href="/create.html"><span class="material-icons" type="button" style="font-size: 48px;">add_circle</span><br>Create</a>
                     <a class="nav-link" href="/socialfeed.html"><span class="material-icons" type="button" style="font-size: 48px;">group</span><br>Feed</a>
                     <a class="nav-link" href="/profile.html"><span class="material-icons" type="button" style="font-size: 48px;">account_circle</span><br>Profile</a>
